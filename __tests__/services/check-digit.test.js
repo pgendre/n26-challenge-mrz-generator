3a57ffb33b7d359a19196e949715dcc71fbc2730
Pierre Gendre <pierre.gendre@wifirst.net>
2019-06-19 22:57:38 +0200

    check digit done

--- __tests__/services/check-digit.test.js
+++ __tests__/services/check-digit.test.js
@@ -1,26 +1,34 @@
 const rewire = require('rewire')
 const service = rewire('../../src/services/check-digit')
 
+const _doesStringFitToFormat = service.__get__('_doesStringFitToFormat')
+const _arrayOfCharIntoArrayOfNumber = service.__get__(
+  '_arrayOfCharIntoArrayOfNumber'
+)
+
 test('SERVICES CHECK DIGIT 01: Should identify a string matching the expected format', () => {
-  const _doesStringFitToFormat = service.__get__('_doesStringFitToFormat')
   const result = _doesStringFitToFormat('ERT43443<<43334<3')
   expect(result).toBe(true)
 })
 
 test('SERVICES CHECK DIGIT 02: Should identify a string NOT matching the expected format (case 1)', () => {
-  const _doesStringFitToFormat = service.__get__('_doesStringFitToFormat')
   const result = _doesStringFitToFormat('ERT4344@<43334<3')
   expect(result).toBe(false)
 })
 
 test('SERVICES CHECK DIGIT 03: Should identify a string NOT matching the expected format (case 2)', () => {
-  const _doesStringFitToFormat = service.__get__('_doesStringFitToFormat')
   const result = _doesStringFitToFormat('ERT4344:<43334<3')
   expect(result).toBe(false)
 })
 
 test('SERVICES CHECK DIGIT 04: Should identify a string NOT matching the expected format (case 3)', () => {
-  const _doesStringFitToFormat = service.__get__('_doesStringFitToFormat')
   const result = _doesStringFitToFormat('EaRT4344<43334<3')
   expect(result).toBe(false)
+})
+
+test('SERVICES CHECK DIGIT 05: Should transform an array of characters into an \
+  array of number (letter converted into associated code) ', () => {
+  const arrayOfChars = ['1', '<', '7', 'A', 'B', '9', 'W']
+  const result = _arrayOfCharIntoArrayOfNumber(arrayOfChars)
+  expect(JSON.stringify(result)).toBe(JSON.stringify([1, 0, 7, 10, 11, 9, 32]))
 })
--- src/services/check-digit.js
+++ src/services/check-digit.js
@@ -1,16 +1,36 @@
 const checkDigitCalculation = inputString => {
   if (!_doesStringFitToFormat(inputString))
     throw new Error('Check Digit : Input string does not match required format')
+  const arrayOfChars = _stringIntoArrayOfChars(inputString)
+  const arrayOfNumbers = _arrayOfCharIntoArrayOfNumber(arrayOfChars)
+  const product = _computeProduct(arrayOfNumbers)
+  return 10 - product % 10
 }
 
+// Formatting / mapping --
+// Example of mathing string: 'EREFGRE45<<<<ER'
 const _doesStringFitToFormat = inputString =>
   /^([A-Z]|[0-9]|<)+$/g.test(inputString)
 
-const _stringIntoArrayOfChar = str => str.split('')
+const _stringIntoArrayOfChars = str => str.split('')
 
 const _arrayOfCharIntoArrayOfNumber = arr =>
-  arr.map(chr => (_isCharANumber(chr) ? chr : _convertCharIntoNumber(chr)))
+  arr.map(
+    chr => (_isCharANumber(chr) ? Number(chr) : _convertCharIntoNumber(chr))
+  )
 
-const _convertCharIntoNumber = chr => chr.charCodeAt(0) - 97
+const _convertCharIntoNumber = chr => (chr === '<' ? 0 : chr.charCodeAt(0) - 55)
 
-const _isCharANumber = c => Number(c) == c
+const _isCharANumber = chr => Number(chr) == chr
+
+// Calculation --
+const _computeProduct = arrayOfNumbers => {
+  let result = 0
+  const weights = [7, 3, 1]
+  arrayOfNumbers.forEach((value, i) => {
+    result += weights[i % 3] * value
+  })
+  return result
+}
+
+module.exports = { checkDigitCalculation }
