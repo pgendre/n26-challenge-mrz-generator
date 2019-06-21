faab03921a1033f291b215a561fa0c09cd96405e
Pierre Gendre <pierre.gendre@wifirst.net>
2019-06-21 17:54:26 +0200

    TD3 OK

--- src/formats/td3/line2.js
+++ src/formats/td3/line2.js
@@ -18,6 +18,8 @@ const generateLine2 = ({ passport, user }) => {
   line2 = _generateDateOfBirth(line2, user)
   line2 = _generateSex(line2, user)
   line2 = _generateExpirationDate(line2, passport)
+  line2 = _generateOptionalField(line2, passport)
+  line2 = _generateGlobalDigitCheck(line2)
   console.log('LINE 2 ==================', line2)
 }
 
@@ -38,5 +40,19 @@ const _generateSex = (line2, user) =>
 
 const _generateExpirationDate = (line2, passport) =>
   generateDateWithCheckDigit(line2, passport.expirationDate, 21)
+
+const _generateOptionalField = (line2, passport) => {
+  let field = truncateString(passport.optionalField1.toUpperCase(), 14)
+  field = replaceSpecialCharsBySpaces(field)
+  line2 = replaceSubStringAtPositionToUpCase(line2, field, 28)
+  const digitCheck = checkDigitCalculation(field)
+  return replaceSubStringAtPositionToUpCase(line2, digitCheck, 42)
+}
+const _generateGlobalDigitCheck = line2 => {
+  let stringToBeChecked =
+    line2.slice(0, 9) + line2.slice(13, 19) + line2.slice(21, 42)
+  const digitCheck = checkDigitCalculation(stringToBeChecked)
+  return replaceSubStringAtPositionToUpCase(line2, digitCheck, 43)
+}
 
 module.exports = { generateLine2 }
