57748813446a259bc5189edde7d79e810de3deb1
Pierre Gendre <pierre.gendre@wifirst.net>
2019-06-23 13:08:05 +0200

    add test for challenge

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -12,6 +12,8 @@ test('LIB GLOBAL 01: Should call export function and generate a complete TD1 MRZ
 
 test('LIB GLOBAL 02: Should call export function and generate a complete TD3 MRZ.', () => {
   const mrz = generateMrz(inputDataForTd3)
+  console.log(`\n\n------------------- TEST FOR CHALLENGE -------------------\n\n \
+              ${mrz.split('\n')[0]} \n ${mrz.split('\n')[1]} \n\n`)
   expect(mrz).toBe(
     'P<FRAGENDRE<<PIERRE<JOSEH<ALEXANDRE<<<<<<<<<\n11AV568680FRA8610175M2105116<<<<<<<<<<<<<<02'
   )
